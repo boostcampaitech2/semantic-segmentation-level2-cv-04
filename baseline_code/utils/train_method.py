b792c9be92ec12ad0b135f626cde2128cc87a498
castleUk <jo631@naver.com>
2021-10-19 18:00:54 +0000

    refactor: Extract method

---
+++ baseline_code/utils/train_method.py
@@ -0,0 +1,102 @@
+from utils.utils import *
+import torch
+import os
+import numpy as np
+
+category_names = ['Background','General trash','Paper','Paper pack','Metal','Glass','Plastic','Styrofoam','Plastic bag','Battery','Clothing']
+
+# TODO 바꿔라이거 
+# FIXME 수정해라 이거
+def save_model(model, saved_dir, file_name='fcn_resnet50_best_model(pretrained).pt'):
+    check_point = {'net': model.state_dict()}
+    output_path = os.path.join(saved_dir, file_name)
+    torch.save(model, output_path)
+
+def train(num_epochs, model, train_loader, val_loader, criterion, optimizer, saved_dir, val_every, device):
+    print(f'Start training..')
+    n_class = 11
+    best_loss = 9999999
+    
+    for epoch in range(num_epochs):
+        model.train()
+
+        hist = np.zeros((n_class, n_class))
+        for step, (images, masks, _) in enumerate(train_loader):
+            images = torch.stack(images)       
+            masks = torch.stack(masks).long() 
+            
+            # gpu 연산을 위해 device 할당
+            images, masks = images.to(device), masks.to(device)
+            
+            # device 할당
+            model = model.to(device)
+            
+            # inference
+            outputs = model(images)
+            
+            # loss 계산 (cross entropy loss)
+            loss = criterion(outputs, masks)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            outputs = torch.argmax(outputs, dim=1).detach().cpu().numpy()
+            masks = masks.detach().cpu().numpy()
+            
+            hist = add_hist(hist, masks, outputs, n_class=n_class)
+            acc, acc_cls, mIoU, fwavacc, IoU = label_accuracy_score(hist)
+            
+            # step 주기에 따른 loss 출력
+            if (step + 1) % 25 == 0:
+                print(f'Epoch [{epoch+1}/{num_epochs}], Step [{step+1}/{len(train_loader)}], \
+                        Loss: {round(loss.item(),4)}, mIoU: {round(mIoU,4)}')
+             
+        # validation 주기에 따른 loss 출력 및 best model 저장
+        if (epoch + 1) % val_every == 0:
+            avrg_loss = validation(epoch + 1, model, val_loader, criterion, device)
+            if avrg_loss < best_loss:
+                print(f"Best performance at epoch: {epoch + 1}")
+                print(f"Save model in {saved_dir}")
+                best_loss = avrg_loss
+                save_model(model, saved_dir)
+
+
+def validation(epoch, model, train_loader, criterion, device):
+    print(f'Start validation #{epoch}')
+    model.eval()
+
+    with torch.no_grad():
+        n_class = 11
+        total_loss = 0
+        cnt = 0
+        
+        hist = np.zeros((n_class, n_class))
+        for step, (images, masks, _) in enumerate(train_loader):
+            
+            images = torch.stack(images)       
+            masks = torch.stack(masks).long()  
+
+            images, masks = images.to(device), masks.to(device)            
+            
+            # device 할당
+            model = model.to(device)
+            
+            outputs = model(images)
+            loss = criterion(outputs, masks)
+            total_loss += loss
+            cnt += 1
+            
+            outputs = torch.argmax(outputs, dim=1).detach().cpu().numpy()
+            masks = masks.detach().cpu().numpy()
+            
+            hist = add_hist(hist, masks, outputs, n_class=n_class)
+        
+        acc, acc_cls, mIoU, fwavacc, IoU = label_accuracy_score(hist)
+        IoU_by_class = [{classes : round(IoU,4)} for IoU, classes in zip(IoU , category_names)]
+        
+        avrg_loss = total_loss / cnt
+        print(f'Validation #{epoch}  Average Loss: {round(avrg_loss.item(), 4)}, Accuracy : {round(acc, 4)}, \
+                mIoU: {round(mIoU, 4)}')
+        print(f'IoU by class : {IoU_by_class}')
+        
+    return avrg_loss
