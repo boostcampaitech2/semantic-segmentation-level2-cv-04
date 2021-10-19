1c0cca0bca4bdef193e33e61852ed83f3c343642
castleUk <jo631@naver.com>
2021-10-19 18:01:04 +0000

    refactor: Extract model

---
+++ baseline_code/model/base_model.py
@@ -0,0 +1,19 @@
+from abc import ABC, ABCMeta
+import segmentation_models_pytorch as smp
+from segmentation_models_pytorch.base.model import SegmentationModel
+
+class BaseModel(metaclass=ABCMeta):
+	def __init__(self, encoderName, encoderWeights="imagenet", imgChannels=3, classes=11):
+			self.model : SegmentationModel = None
+
+	def __call__(self,*args,**kwargs):
+			self.model(*args,**kwargs)
+
+class UNetPlusPlus(BaseModel):
+	def __init__(self, encoderName, encoderWeights="imagenet", imgChannels=3, classes=11):
+		self.model = smp.UnetPlusPlus(
+			encoder_name=encoderName,
+			encoder_weights=encoderWeights,
+			in_channels=imgChannels,
+			classes=classes
+		)
