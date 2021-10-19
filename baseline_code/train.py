5be2aa9af0f8f9700f96e8714d07cadee1267150
castleUk <jo631@naver.com>
2021-10-19 18:01:12 +0000

    refactor: Extract train.py

--- baseline_code/train.py
+++ baseline_code/train.py
@@ -1,6 +1,13 @@
 import torch
 import argparse
 
+from model.base_model import UNetPlusPlus
+from dataset.base_dataset import CustomDataset
+from dataloader.base_dataloader import BaseDataLoader
+from utils.train_method import train
+from torch import nn
+import os
+
 
 def getArg():
 	parser = argparse.ArgumentParser()
@@ -9,7 +16,45 @@ def getArg():
 	parser.add_argument('--epoch', default=20,type=int, required=False)
 	parser.add_argument('--lr',default=1e-5,type=float, required=False)
 	parser.add_argument('--seed', default=21,type=int,  required=False)
+	parser.add_argument('--save_period', default=1, type=int, required=False)
+
+	parser.add_argument('--image_root', default="../input/data", type=str, required=False)
+	parser.add_argument('--train_json', default="train.json", type=str, required=False)
+	parser.add_argument('--val_json', default="val.json", type=str, required=False)
+	# parser.add_argument('--train_json', default="/test.json", type=str, required=False)
+
+	parser.add_argument('--output_path', default="../output", type=str, required=False)
+
 	return parser.parse_known_args()
+
+def main(arg):
+
+	print('pytorch version: {}'.format(torch.__version__))
+	print('GPU 사용 가능 여부: {}'.format(torch.cuda.is_available()))
+	device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+	if not os.path.isdir(arg.output_path):                                                           
+		os.mkdir(arg.output_path)
+
+	from transform import train_transform, val_transform
+
+	train_dataset = CustomDataset(data_dir=addPath([arg.image_root,arg.train_json]),image_root=arg.image_root, mode='train', transform=train_transform)
+	val_dataset = CustomDataset(data_dir=addPath([arg.image_root,arg.val_json]),image_root=arg.image_root, mode='val', transform=val_transform)
+
+	trainLoader = BaseDataLoader(dataset=train_dataset, batch_size=arg.batch,shuffle=True,num_workers=4)
+	valLoader = BaseDataLoader(dataset=val_dataset, batch_size=arg.batch,shuffle=False,num_workers=4)
+
+	model = UNetPlusPlus(encoderName="timm-efficientnet-b8").model
+
+	# Loss function 정의
+	criterion = nn.CrossEntropyLoss()
+
+	# Optimizer 정의
+	optimizer = torch.optim.Adam(params = model.parameters(), lr = arg.lr, weight_decay=1e-5)
+
+	# TODO 이름 넣어라
+	train(arg.epoch, model, trainLoader, valLoader, criterion, optimizer, arg.output_path, arg.save_period, device)
 
 def setSeed(seed):
 	import numpy as np
@@ -22,276 +67,8 @@ def setSeed(seed):
 	np.random.seed(seed)
 	random.seed(seed)
 
-
-##TODO 수정해야됨!!!
-category_names = ['Background','General trash','Paper','Paper pack','Metal','Glass','Plastic','Styrofoam','Plastic bag','Battery','Clothing']
-
-def get_classname(classID, cats):
-    for i in range(len(cats)):
-        if cats[i]['id']==classID:
-            return cats[i]['name']
-    return "None"
-
-from torch.utils.data import Dataset, DataLoader
-from pycocotools.coco import COCO
-import cv2
-import os
-dataset_path  = '../input/data'
-anns_file_path = dataset_path + '/' + 'train_all.json'
-import numpy as np
-
-class CustomDataLoader(Dataset):
-    """COCO format"""
-    def __init__(self, data_dir, mode = 'train', transform = None):
-        super().__init__()
-        self.mode = mode
-        self.transform = transform
-        self.coco = COCO(data_dir)
-        
-    def __getitem__(self, index: int):
-        # dataset이 index되어 list처럼 동작
-        image_id = self.coco.getImgIds(imgIds=index)
-        image_infos = self.coco.loadImgs(image_id)[0]
-        
-        # cv2 를 활용하여 image 불러오기
-        images = cv2.imread(os.path.join(dataset_path, image_infos['file_name']))
-        images = cv2.cvtColor(images, cv2.COLOR_BGR2RGB).astype(np.float32)
-        images /= 255.0
-        
-        if (self.mode in ('train', 'val')):
-            ann_ids = self.coco.getAnnIds(imgIds=image_infos['id'])
-            anns = self.coco.loadAnns(ann_ids)
-
-            # Load the categories in a variable
-            cat_ids = self.coco.getCatIds()
-            cats = self.coco.loadCats(cat_ids)
-
-            # masks : size가 (height x width)인 2D
-            # 각각의 pixel 값에는 "category id" 할당
-            # Background = 0
-            masks = np.zeros((image_infos["height"], image_infos["width"]))
-            # General trash = 1, ... , Cigarette = 10
-            anns = sorted(anns, key=lambda idx : len(idx['segmentation'][0]), reverse=False)
-            for i in range(len(anns)):
-                className = get_classname(anns[i]['category_id'], cats)
-                pixel_value = category_names.index(className)
-                masks[self.coco.annToMask(anns[i]) == 1] = pixel_value
-            masks = masks.astype(np.int8)
-                        
-            # transform -> albumentations 라이브러리 활용
-            if self.transform is not None:
-                transformed = self.transform(image=images, mask=masks)
-                images = transformed["image"]
-                masks = transformed["mask"]
-            return images, masks, image_infos
-        
-        if self.mode == 'test':
-            # transform -> albumentations 라이브러리 활용
-            if self.transform is not None:
-                transformed = self.transform(image=images)
-                images = transformed["image"]
-            return images, image_infos
-    
-    def __len__(self) -> int:
-        # 전체 dataset의 size를 return
-        return len(self.coco.getImgIds())
-
-# train.json / validation.json / test.json 디렉토리 설정
-train_path = dataset_path + '/train.json'
-val_path = dataset_path + '/val.json'
-test_path = dataset_path + '/test.json'
-
-# collate_fn needs for batch
-def collate_fn(batch):
-    return tuple(zip(*batch))
-
-
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
-
-train_transform = A.Compose([
-                            A.Flip(),
-                            A.RandomCrop(width=256, height=256),
-                            A.RandomBrightnessContrast(p=0.2),
-                            # A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                            ToTensorV2(),
-                            ])
-
-val_transform = A.Compose([
-                            # A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                          ToTensorV2(),
-                          ])
-
-test_transform = A.Compose([
-                            # A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                           ToTensorV2(),
-                           ])
-
-train_dataset = CustomDataLoader(data_dir=train_path, mode='train', transform=train_transform)
-
-# validation dataset
-val_dataset = CustomDataLoader(data_dir=val_path, mode='val', transform=val_transform)
-
-# test dataset
-test_dataset = CustomDataLoader(data_dir=test_path, mode='test', transform=test_transform)
-
-
-
-														
-import segmentation_models_pytorch as smp
-
-model = smp.UnetPlusPlus(
-    encoder_name="timm-efficientnet-b2",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
-    encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
-    in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
-    classes=11,                      # model output channels (number of classes in your dataset)
-)
-
-from utils import *
-
-def train(num_epochs, model, train_loader, val_loader, criterion, optimizer, saved_dir, val_every, device):
-    print(f'Start training..')
-    n_class = 11
-    best_loss = 9999999
-    
-    for epoch in range(num_epochs):
-        model.train()
-
-        hist = np.zeros((n_class, n_class))
-        for step, (images, masks, _) in enumerate(train_loader):
-            images = torch.stack(images)       
-            masks = torch.stack(masks).long() 
-            
-            # gpu 연산을 위해 device 할당
-            images, masks = images.to(device), masks.to(device)
-            
-            # device 할당
-            model = model.to(device)
-            
-            # inference
-            outputs = model(images)
-            
-            # loss 계산 (cross entropy loss)
-            loss = criterion(outputs, masks)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            outputs = torch.argmax(outputs, dim=1).detach().cpu().numpy()
-            masks = masks.detach().cpu().numpy()
-            
-            hist = add_hist(hist, masks, outputs, n_class=n_class)
-            acc, acc_cls, mIoU, fwavacc, IoU = label_accuracy_score(hist)
-            
-            # step 주기에 따른 loss 출력
-            if (step + 1) % 25 == 0:
-                print(f'Epoch [{epoch+1}/{num_epochs}], Step [{step+1}/{len(train_loader)}], \
-                        Loss: {round(loss.item(),4)}, mIoU: {round(mIoU,4)}')
-             
-        # validation 주기에 따른 loss 출력 및 best model 저장
-        if (epoch + 1) % val_every == 0:
-            avrg_loss = validation(epoch + 1, model, val_loader, criterion, device)
-            if avrg_loss < best_loss:
-                print(f"Best performance at epoch: {epoch + 1}")
-                print(f"Save model in {saved_dir}")
-                best_loss = avrg_loss
-                save_model(model, saved_dir)
-
-
-def validation(epoch, model, train_loader, criterion, device):
-    print(f'Start validation #{epoch}')
-    model.eval()
-
-    with torch.no_grad():
-        n_class = 11
-        total_loss = 0
-        cnt = 0
-        
-        hist = np.zeros((n_class, n_class))
-        for step, (images, masks, _) in enumerate(train_loader):
-            
-            images = torch.stack(images)       
-            masks = torch.stack(masks).long()  
-
-            images, masks = images.to(device), masks.to(device)            
-            
-            # device 할당
-            model = model.to(device)
-            
-            outputs = model(images)
-            loss = criterion(outputs, masks)
-            total_loss += loss
-            cnt += 1
-            
-            outputs = torch.argmax(outputs, dim=1).detach().cpu().numpy()
-            masks = masks.detach().cpu().numpy()
-            
-            hist = add_hist(hist, masks, outputs, n_class=n_class)
-        
-        acc, acc_cls, mIoU, fwavacc, IoU = label_accuracy_score(hist)
-        IoU_by_class = [{classes : round(IoU,4)} for IoU, classes in zip(IoU , category_names)]
-        
-        avrg_loss = total_loss / cnt
-        print(f'Validation #{epoch}  Average Loss: {round(avrg_loss.item(), 4)}, Accuracy : {round(acc, 4)}, \
-                mIoU: {round(mIoU, 4)}')
-        print(f'IoU by class : {IoU_by_class}')
-        
-    return avrg_loss
-
-# 모델 저장 함수 정의
-val_every = 1
-
-saved_dir = './saved'
-if not os.path.isdir(saved_dir):                                                           
-    os.mkdir(saved_dir)
-
-def save_model(model, saved_dir, file_name='fcn_resnet50_best_model(pretrained).pt'):
-    check_point = {'net': model.state_dict()}
-    output_path = os.path.join(saved_dir, file_name)
-    torch.save(model, output_path)
-
-
-
-
-
-def main(arg):
-
-	print('pytorch version: {}'.format(torch.__version__))
-	print('GPU 사용 가능 여부: {}'.format(torch.cuda.is_available()))
-	print(torch.cuda.get_device_name(0))
-	print(torch.cuda.device_count())
-	device = "cuda" if torch.cuda.is_available() else "cpu"
-
-	# Dataloader
-
-	# DataLoader
-	train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-																						batch_size=arg.batch,
-																						shuffle=True,
-																						num_workers=4,
-																						collate_fn=collate_fn)
-
-	val_loader = torch.utils.data.DataLoader(dataset=val_dataset, 
-																					batch_size=arg.batch,
-																					shuffle=False,
-																					num_workers=4,
-																					collate_fn=collate_fn)
-
-	test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-																						batch_size=arg.batch,
-																						num_workers=4,
-																						collate_fn=collate_fn)
-
-
-	from torch import nn
-
-	# Loss function 정의
-	criterion = nn.CrossEntropyLoss()
-
-	# Optimizer 정의
-	optimizer = torch.optim.Adam(params = model.parameters(), lr = arg.lr, weight_decay=1e-5)
-
-	train(arg.epoch, model, train_loader, val_loader, criterion, optimizer, saved_dir, val_every, device)
+def addPath(pathList):
+	return os.path.join(*pathList)
 
 if __name__=="__main__":
 	arg, _ = getArg()
