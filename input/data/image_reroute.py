0873862b70c63d7c60c645736812fa009d505a58
8639sung <8639sung@gmail.com>
2021-10-19 13:48:47 +0000

    feat: Add image reroute function

---
+++ input/data/image_reroute.py
@@ -0,0 +1,53 @@
+import os
+import json
+import shutil
+
+DATAROOT = "./"
+TRAINJSON = os.path.join(DATAROOT,"train.json")
+VALIDJSON = os.path.join(DATAROOT,"val.json")
+TESTJSON = os.path.join(DATAROOT,"test.json")
+
+'''
+Redistribution image by train/valid/test 
+
+rename img by img_id
+'''
+def _rename_images(json_dir, image_dir):
+	with open(json_dir, "r", encoding="utf8") as outfile:
+		json_data = json.load(outfile)
+	image_datas = json_data["images"]
+
+	for image_data in image_datas:
+
+		shutil.copyfile(os.path.join(image_data['file_name']), os.path.join(image_dir,f"{image_data['id']:04}.jpg"))
+
+'''
+Make dir
+'''
+def _make_directory(paths):
+	for path in paths:
+		os.makedirs(path, exist_ok=True)
+
+
+'''
+Wrap func
+'''
+def make(json,path):
+	imagePath = '../mmseg/images/'+path
+
+	_make_directory(imagePath)
+	_rename_images(json,imagePath)
+
+
+'''
+Main
+'''
+def __main__():
+
+	make(TRAINJSON, 'training')
+	make(VALIDJSON, 'validation')
+	make(TESTJSON, 'test')
+
+
+if __name__=='__main__':
+	__main__()
