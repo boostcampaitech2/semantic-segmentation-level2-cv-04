d02c0b5f4157b72f1c74b2214995ceed86362b62
8639sung <8639sung@gmail.com>
2021-10-20 01:43:56 +0000

    fix: Fix labeling error

--- mmsegmentation/configs/_custom_/datasets/dataset.py
+++ mmsegmentation/configs/_custom_/datasets/dataset.py
@@ -17,7 +17,7 @@ img_norm_cfg = dict(
 crop_size = (512, 512)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', reduce_zero_label=True),
+    dict(type='LoadAnnotations'),
     dict(type='Resize', img_scale=(512, 512), ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
@@ -66,5 +66,5 @@ data = dict(
         palette=palette,
         type=dataset_type,
         #reduce_zero_label=True,
-        img_dir=data_root + "images",
+        img_dir=data_root + "test",
         pipeline=test_pipeline))
--- mmsegmentation/train.sh
+++ mmsegmentation/train.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python tools/train.py configs/_custom_/models/hr48_512x512.py --work-dir ../output/mmseg/hr48_512x512
+conda activate mmseg
+
+python tools/train.py configs/_custom_/models/hr48_512x512.py --seed 42 --work-dir ../output/mmseg/hr48_512x512 
