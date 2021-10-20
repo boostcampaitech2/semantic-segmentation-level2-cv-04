61fc9066b6c15ed5c227c491e875632f274da800
castleUk <jo631@naver.com>
2021-10-20 12:18:08 +0000

    feat: Change save method

--- baseline_code/train.py
+++ baseline_code/train.py
@@ -1,7 +1,7 @@
 import torch
 import argparse
 
-from model.base_model import UNetPlusPlus
+from model.base_model import *
 from dataset.base_dataset import CustomDataset
 from dataloader.base_dataloader import BaseDataLoader
 from utils.train_method import train
@@ -16,7 +16,7 @@ def getArg():
 	parser.add_argument('--epoch', default=20,type=int, required=False)
 	parser.add_argument('--lr',default=1e-5,type=float, required=False)
 	parser.add_argument('--seed', default=21,type=int,  required=False)
-	parser.add_argument('--save_period', default=1, type=int, required=False)
+	parser.add_argument('--save_capacity', default=5, type=int, required=False) ## Best 모델 몇개나 저장할것인지
 
 	parser.add_argument('--image_root', default="../input/data", type=str, required=False)
 	parser.add_argument('--train_json', default="train.json", type=str, required=False)
@@ -24,6 +24,8 @@ def getArg():
 	# parser.add_argument('--train_json', default="/test.json", type=str, required=False)
 
 	parser.add_argument('--output_path', default="../output", type=str, required=False)
+
+	parser.add_argument('--custom_name', type=str ,required=True)
 
 	return parser.parse_known_args()
 
@@ -33,9 +35,9 @@ def main(arg):
 	print('GPU 사용 가능 여부: {}'.format(torch.cuda.is_available()))
 	device = "cuda" if torch.cuda.is_available() else "cpu"
 
-
-	if not os.path.isdir(arg.output_path):                                                           
-		os.mkdir(arg.output_path)
+	outputPath = os.path.join(arg.output_path, arg.custom_name)
+	if not os.path.isdir(outputPath):
+		os.makedirs(outputPath, exist_ok=True)
 
 	from transform import train_transform, val_transform
 
@@ -45,16 +47,15 @@ def main(arg):
 	trainLoader = BaseDataLoader(dataset=train_dataset, batch_size=arg.batch,shuffle=True,num_workers=4)
 	valLoader = BaseDataLoader(dataset=val_dataset, batch_size=arg.batch,shuffle=False,num_workers=4)
 
-	model = UNetPlusPlus(encoderName="timm-efficientnet-b8").model
-
+	# model = DeepLabV3Plus(encoderName="timm-efficientnet-b7").model
+	model = UNetPlusPlus(encoderName="timm-efficientnet-b1").model
 	# Loss function 정의
 	criterion = nn.CrossEntropyLoss()
 
 	# Optimizer 정의
 	optimizer = torch.optim.Adam(params = model.parameters(), lr = arg.lr, weight_decay=1e-5)
 
-	# TODO 이름 넣어라
-	train(arg.epoch, model, trainLoader, valLoader, criterion, optimizer, arg.output_path, arg.save_period, device)
+	train(arg.epoch, model, trainLoader, valLoader, criterion, optimizer, outputPath, arg.save_capacity, device)
 
 def setSeed(seed):
 	import numpy as np
