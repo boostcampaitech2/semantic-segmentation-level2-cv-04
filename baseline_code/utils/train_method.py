adc96b658d2a7a2804c29b0bc88da70043013408
castleUk <jo631@naver.com>
2021-10-20 06:17:38 +0000

    feat: Add tqdm

--- baseline_code/utils/train_method.py
+++ baseline_code/utils/train_method.py
@@ -2,10 +2,10 @@ from utils.utils import *
 import torch
 import os
 import numpy as np
+from tqdm import tqdm
 
 category_names = ['Background','General trash','Paper','Paper pack','Metal','Glass','Plastic','Styrofoam','Plastic bag','Battery','Clothing']
 
-# TODO 바꿔라이거 
 # FIXME 수정해라 이거
 def save_model(model, saved_dir, file_name='fcn_resnet50_best_model(pretrained).pt'):
     check_point = {'net': model.state_dict()}
@@ -20,8 +20,10 @@ def train(num_epochs, model, train_loader, val_loader, criterion, optimizer, sav
     for epoch in range(num_epochs):
         model.train()
 
+        pbar = tqdm(train_loader,desc=f"train epoch {epoch} ")
+
         hist = np.zeros((n_class, n_class))
-        for step, (images, masks, _) in enumerate(train_loader):
+        for step, (images, masks, _) in enumerate(pbar):
             images = torch.stack(images)       
             masks = torch.stack(masks).long() 
             
@@ -45,12 +47,10 @@ def train(num_epochs, model, train_loader, val_loader, criterion, optimizer, sav
             
             hist = add_hist(hist, masks, outputs, n_class=n_class)
             acc, acc_cls, mIoU, fwavacc, IoU = label_accuracy_score(hist)
-            
-            # step 주기에 따른 loss 출력
-            if (step + 1) % 25 == 0:
-                print(f'Epoch [{epoch+1}/{num_epochs}], Step [{step+1}/{len(train_loader)}], \
-                        Loss: {round(loss.item(),4)}, mIoU: {round(mIoU,4)}')
-             
+
+            pbar.set_postfix_str(f"Acc: {acc.item():.2f}, AccCls: {acc_cls.item():.2f}, fwavacc: {fwavacc.item():.2f}, Loss: {loss.item():.2f}, mIoU: {mIoU.item():.2f}")
+
+        #TODO 저장주기 바까
         # validation 주기에 따른 loss 출력 및 best model 저장
         if (epoch + 1) % val_every == 0:
             avrg_loss = validation(epoch + 1, model, val_loader, criterion, device)
@@ -71,7 +71,10 @@ def validation(epoch, model, train_loader, criterion, device):
         cnt = 0
         
         hist = np.zeros((n_class, n_class))
-        for step, (images, masks, _) in enumerate(train_loader):
+
+        pbar = tqdm(train_loader,desc=f"validating ")
+        IoU_by_class = None
+        for step, (images, masks, _) in enumerate(pbar):
             
             images = torch.stack(images)       
             masks = torch.stack(masks).long()  
@@ -90,13 +93,12 @@ def validation(epoch, model, train_loader, criterion, device):
             masks = masks.detach().cpu().numpy()
             
             hist = add_hist(hist, masks, outputs, n_class=n_class)
+            acc, acc_cls, mIoU, fwavacc, IoU = label_accuracy_score(hist)
+            pbar.set_postfix_str(f"Acc: {acc.item():.2f}, AccCls: {acc_cls.item():.2f}, fwavacc: {fwavacc.item():.2f}, Loss: {loss.item():.2f}, mIoU: {mIoU.item():.2f}")
         
-        acc, acc_cls, mIoU, fwavacc, IoU = label_accuracy_score(hist)
         IoU_by_class = [{classes : round(IoU,4)} for IoU, classes in zip(IoU , category_names)]
         
         avrg_loss = total_loss / cnt
-        print(f'Validation #{epoch}  Average Loss: {round(avrg_loss.item(), 4)}, Accuracy : {round(acc, 4)}, \
-                mIoU: {round(mIoU, 4)}')
-        print(f'IoU by class : {IoU_by_class}')
+        print(f'Average Loss : {avrg_loss.item():.4f}, IoU by class : {IoU_by_class}')
         
     return avrg_loss
