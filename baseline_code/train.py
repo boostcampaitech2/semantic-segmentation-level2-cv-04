78b5465637eb4ba5a1d6d70d86048e3126f6ef67
castleUk <jo631@naver.com>
2021-10-20 16:42:44 +0000

    feat: Add wandb

--- baseline_code/train.py
+++ baseline_code/train.py
@@ -8,24 +8,30 @@ from utils.train_method import train
 from torch import nn
 import os
 
+import wandb
 
 def getArg():
 	parser = argparse.ArgumentParser()
 
 	parser.add_argument('--batch', default=16,type=int, required=False)
 	parser.add_argument('--epoch', default=20,type=int, required=False)
-	parser.add_argument('--lr',default=1e-5,type=float, required=False)
+	parser.add_argument('--lr',default=1e-3,type=float, required=False)
 	parser.add_argument('--seed', default=21,type=int,  required=False)
 	parser.add_argument('--save_capacity', default=5, type=int, required=False) ## Best 모델 몇개나 저장할것인지
 
 	parser.add_argument('--image_root', default="../input/data", type=str, required=False)
 	parser.add_argument('--train_json', default="train.json", type=str, required=False)
 	parser.add_argument('--val_json', default="val.json", type=str, required=False)
+	parser.add_argument('--output_path', default="../output", type=str, required=False)
 	# parser.add_argument('--train_json', default="/test.json", type=str, required=False)
 
-	parser.add_argument('--output_path', default="../output", type=str, required=False)
-
 	parser.add_argument('--custom_name', type=str ,required=True)
+	parser.add_argument('--train_worker',type=int, default=4,required=False)
+	parser.add_argument('--valid_worker',type=int, default=4,required=False)
+
+	parser.add_argument('--wandb',action="store_true", default=False)
+	parser.add_argument('--wandb_project', default='segmentation',type=str, required=False)
+	parser.add_argument('--wandb_entity', default='cv4',type=str, required=False)
 
 	return parser.parse_known_args()
 
@@ -36,26 +42,35 @@ def main(arg):
 	device = "cuda" if torch.cuda.is_available() else "cpu"
 
 	outputPath = os.path.join(arg.output_path, arg.custom_name)
-	if not os.path.isdir(outputPath):
-		os.makedirs(outputPath, exist_ok=True)
+	os.makedirs(outputPath, exist_ok=False)
+
 
 	from transform import train_transform, val_transform
 
 	train_dataset = CustomDataset(data_dir=addPath([arg.image_root,arg.train_json]),image_root=arg.image_root, mode='train', transform=train_transform)
 	val_dataset = CustomDataset(data_dir=addPath([arg.image_root,arg.val_json]),image_root=arg.image_root, mode='val', transform=val_transform)
 
-	trainLoader = BaseDataLoader(dataset=train_dataset, batch_size=arg.batch,shuffle=True,num_workers=4)
-	valLoader = BaseDataLoader(dataset=val_dataset, batch_size=arg.batch,shuffle=False,num_workers=4)
+	trainLoader = BaseDataLoader(dataset=train_dataset, batch_size=arg.batch,shuffle=True,num_workers=arg.train_worker)
+	valLoader = BaseDataLoader(dataset=val_dataset, batch_size=arg.batch,shuffle=False,num_workers=arg.valid_worker)
 
-	# model = DeepLabV3Plus(encoderName="timm-efficientnet-b7").model
-	model = UNetPlusPlus(encoderName="timm-efficientnet-b1").model
+	# TODO 모델하고 argument 따로 빼놓기
+	model = DeepLabV3Plus(encoderName="timm-efficientnet-b7").model
+	# model = UNetPlusPlus(encoderName="timm-efficientnet-b1").model
 	# Loss function 정의
 	criterion = nn.CrossEntropyLoss()
 
 	# Optimizer 정의
-	optimizer = torch.optim.Adam(params = model.parameters(), lr = arg.lr, weight_decay=1e-5)
+	optimizer = torch.optim.Adam(params = model.parameters(), lr = arg.lr, weight_decay=1e-4)
+	scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,T_max=100,eta_min=1e-4)
+	# wandb
 
-	train(arg.epoch, model, trainLoader, valLoader, criterion, optimizer, outputPath, arg.save_capacity, device)
+	if arg.wandb:
+			wandb.login()
+			wandb.init(project=arg.wandb_project, entity=arg.wandb_entity, name=arg.custom_name, config=arg)
+			wandb.watch(model,criterion, log="all",log_freq=10)
+
+
+	train(arg.epoch, model, trainLoader, valLoader, criterion, optimizer,scheduler, outputPath, arg.save_capacity, device, arg.wandb)
 
 def setSeed(seed):
 	import numpy as np
