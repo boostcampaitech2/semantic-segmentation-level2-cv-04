02b87d638d8f49a6baf911ac310153d4e57dd4d7
8639sung <8639sung@gmail.com>
2021-10-20 12:16:45 +0000

    feat: Add swin_b pretrain model

---
+++ mmsegmentation/pretrain/README.md
@@ -0,0 +1,3 @@
+Download pretrain model
+
+swin_b : https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224.pth
--- mmsegmentation/train.sh
+++ mmsegmentation/train.sh
@@ -2,4 +2,6 @@
 
 # python tools/train.py configs/_custom_/models/hr48_512x512.py --seed 42 --work-dir ../output/mmseg/hr48_512x512 
 
-python tools/train.py configs/_custom_/models/hr48_512x512.py --seed 42 --work-dir ../output/mmseg/hr48_0
+# python tools/train.py configs/_custom_/models/hr48.py --seed 42 --work-dir ../output/mmseg/hr48_0
+
+python tools/train.py configs/_custom_/models/upernet_swin_b.py --seed 42 --work-dir ../output/mmseg/upernet_swin_b_0
