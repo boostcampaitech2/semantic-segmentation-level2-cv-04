71a2b33651f00cfa343afd4d0c369e92afd5b9de
castleUk <jo631@naver.com>
2021-10-21 08:54:57 +0000

    feat: Model save helper

--- baseline_code/utils/save_helper.py
+++ baseline_code/utils/save_helper.py
@@ -3,12 +3,13 @@ import os
 import torch
 
 class SaveHelper:
-	def __init__(self, capacity) -> None:
+	def __init__(self, capacity, saved_dir) -> None:
 		self.savedList = deque()
 		self.capacity = max(capacity,2)
 		self.bestEpoch = 0
 		self.bestLoss = 9999999
-	
+		self.savedDir = saved_dir
+
 	@staticmethod
 	def _fileFormat(epoch):
 		return f"epoch{epoch}.pth"
@@ -24,16 +25,22 @@ class SaveHelper:
 
 		return ok
 	
-	def removeModel(self,saved_dir):
+	def _concatSaveDir(self, fileName):
+		return os.path.join(self.savedDir,fileName)
+
+	def _concatSaveDirByEpoch(self, epoch):
+		return self._concatSaveDir(self._fileFormat(epoch))
+
+	def removeModel(self):
 
 		if len(self.savedList) <= self.capacity:
 			return
 
 		delTarget = self.savedList.popleft()
-		os.remove(os.path.join(saved_dir,self._fileFormat(delTarget)))
+		os.remove(self._concatSaveDirByEpoch(delTarget))
 	
-	def saveModel(self,epoch, saved_dir, model):
-		torch.save(model.state_dict(), os.path.join(saved_dir,self._fileFormat(epoch)))
+	def saveModel(self,epoch, model):
+		torch.save(model.state_dict(), self._concatSaveDirByEpoch(epoch))
   
 	def renameBestModel(self):
-		os.rename(self._fileFormat(self.bestEpoch),"best.pth")
+		os.rename(self._concatSaveDirByEpoch(self.bestEpoch),self._concatSaveDir("best.pth"))
