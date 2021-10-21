29fc957b54c4ba34b91630b337dc36f30761a794
castleUk <jo631@naver.com>
2021-10-21 06:03:01 +0000

    refactor: Extract tqdm method

---
+++ baseline_code/utils/tqdm.py
@@ -0,0 +1,44 @@
+from tqdm import tqdm
+
+class TQDM:
+
+	@classmethod
+	def _makeProcessBar(cls,iterable,desc):
+		return tqdm(iterable,desc=desc)
+
+	@classmethod
+	def makeMainProcessBar(cls, epoch):
+		return cls._makeProcessBar(range(epoch),desc="Start Training")
+	
+	@classmethod
+	def makePbar(cls, loader ,epoch, isTrain):
+		if isTrain:
+			desc = f"Validation #{epoch} "
+		else:
+			desc = f"Train #{epoch} "
+		
+		return cls._makeProcessBar(loader,desc)
+	
+	@classmethod
+	def _setDesctiption(cls, pbar, text):
+		pbar.set_description_str(text)
+
+	@classmethod
+	def setMainPbarDescInSaved(cls,pbar ,epoch, mIoU):
+		cls._setDesctiption(pbar,f"Last save epoch #{epoch}, mIoU: {mIoU:.3f}")
+	
+	@classmethod
+	def _setPostfix(cls, pbar, text):
+		pbar.set_postfix_str(text)
+
+	@classmethod
+	def setMainPbarPostInValid(cls,pbar,avrgLoss):
+		cls._setPostfix(pbar,f"Avrg Loss: {avrgLoss.item():.2f}")
+	
+	@classmethod
+	def setPbarPostInStep(cls, pbar, acc, clsAccMean, loss, mIoU):
+		cls._setPostfix(pbar, 
+			f"Acc: {acc.item():.2f}, AccCls: {clsAccMean.item():.2f}, Loss: {loss.item():.2f}, mIoU: {mIoU.item():.2f}")
+
+
+	
--- baseline_code/utils/train_method.py
+++ baseline_code/utils/train_method.py
@@ -2,9 +2,9 @@ from utils.utils import *
 import torch
 import os
 import numpy as np
-from tqdm import tqdm
 from collections import deque
 from utils.wandb_method import WandBMethod
+from utils.tqdm import TQDM
 
 category_names = ['Background','General trash','Paper','Paper pack','Metal','Glass','Plastic','Styrofoam','Plastic bag','Battery','Clothing']
 
@@ -15,11 +15,11 @@ def train(num_epochs, model, train_loader, val_loader, criterion, optimizer, sch
     savedEpochList = deque()
     bestEpoch=0
     # IoU_by_class = [{classes : round(IoU,2)} for IoU, classes in zip([0 for i in range(len(category_names))], category_names)]  
-    mainPbar = tqdm(range(num_epochs), desc="Start Training")
+    mainPbar = TQDM.makeMainProcessBar(num_epochs)
 
     for epoch in mainPbar:
         model.train()
-        pbar = tqdm(train_loader, desc=f"Train #{epoch} ")
+        pbar = TQDM.makePbar(train_loader, epoch, True)
 
         hist = np.zeros((n_class, n_class))
         for step, (images, masks, _) in enumerate(pbar):
@@ -49,7 +49,7 @@ def train(num_epochs, model, train_loader, val_loader, criterion, optimizer, sch
             acc, acc_cls, acc_clsmean, mIoU, fwavacc, IoU = label_accuracy_score(hist)
 
             # pbar.update()
-            pbar.set_postfix_str(f"Acc: {acc.item():.2f}, AccCls: {acc_clsmean.item():.2f}, fwavacc: {fwavacc.item():.2f}, Loss: {loss.item():.2f}, mIoU: {mIoU.item():.2f}")
+            TQDM.setPbarPostInStep(pbar, acc,acc_clsmean,loss,mIoU)
 
             if doWandb:
                 WandBMethod.trainLog(loss, acc, scheduler.get_last_lr())
@@ -57,7 +57,7 @@ def train(num_epochs, model, train_loader, val_loader, criterion, optimizer, sch
         avrg_loss , mIoU= validation(epoch, model, val_loader, criterion, device, mainPbar, doWandb)
         
         if avrg_loss < best_loss:
-            mainPbar.set_description_str(f"Last save epoch #{epoch}, mIoU: {mIoU:.3f}")
+            TQDM.setMainPbarDescInSaved(mainPbar,epoch,mIoU)
             best_loss = avrg_loss
 
             savedEpochList.append(epoch)
@@ -71,7 +71,7 @@ def train(num_epochs, model, train_loader, val_loader, criterion, optimizer, sch
             torch.save(model.state_dict(), os.path.join(saved_dir,f"epoch{epoch}.pth"))
     os.rename(f"epoch{bestEpoch}.pth","best.pth")
 
-def validation(epoch, model, train_loader, criterion, device, mainPbar, doWandb):
+def validation(epoch, model, valid_loader, criterion, device, mainPbar, doWandb):
     model.eval()
     with torch.no_grad():
         n_class = 11
@@ -80,7 +80,8 @@ def validation(epoch, model, train_loader, criterion, device, mainPbar, doWandb)
         
         hist = np.zeros((n_class, n_class))
 
-        pbar = tqdm(train_loader,desc=f"Validation #{epoch} ")
+        pbar = TQDM.makePbar(valid_loader,epoch,False)
+
         for step, (images, masks, _) in enumerate(pbar):
             
             images = torch.stack(images)       
@@ -102,13 +103,11 @@ def validation(epoch, model, train_loader, criterion, device, mainPbar, doWandb)
             hist = add_hist(hist, masks, outputs, n_class=n_class)
             acc, acc_cls, acc_clsmean, mIoU, fwavacc, IoU = label_accuracy_score(hist)
 
-            pbar.set_postfix_str(f"Acc: {acc.item():.2f}, AccCls: {acc_clsmean.item():.2f}, fwavacc: {fwavacc.item():.2f}, Loss: {loss.item():.2f}, mIoU: {mIoU.item():.2f}")
+            TQDM.setPbarPostInStep(pbar,acc,acc_clsmean,loss,mIoU)
 
         if doWandb:
             WandBMethod.validLog(IoU, acc_cls, acc_clsmean, acc, mIoU, images, outputs, masks, {i:category for i, category in enumerate(category_names)})
-
       
         avrg_loss = total_loss / cnt
-        mainPbar.set_postfix_str(f"Avrg Loss: {avrg_loss.item():.2f}")
-        
+        TQDM.setMainPbarPostInValid(mainPbar,avrg_loss)
     return avrg_loss, mIoU
