ea9cf91911f1c4f73d30252ef3c79233c5d74019
castleUk <jo631@naver.com>
2021-10-22 08:49:19 +0000

    fix: 대회 규정대로 수정

--- baseline_code/dataset/base_dataset.py
+++ baseline_code/dataset/base_dataset.py
@@ -44,7 +44,8 @@ class CustomDataset(Dataset):
             # Background = 0
             masks = np.zeros((image_infos["height"], image_infos["width"]))
             # General trash = 1, ... , Cigarette = 10
-            anns = sorted(anns, key=lambda idx : len(idx['segmentation'][0]), reverse=True)
+            anns = sorted(anns, key=lambda idx : idx['area'], reverse=True)
+            
             for i in range(len(anns)):
                 className = get_classname(anns[i]['category_id'], cats)
                 pixel_value = category_names.index(className)
