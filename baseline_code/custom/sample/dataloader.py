0aaf678ed9d9123f5e4585f78b202f90fa7bbc42
castleUk <jo631@naver.com>
2021-10-22 11:24:13 +0000

    chore: Sample 더 가볍게

--- baseline_code/custom/sample/dataloader.py
+++ baseline_code/custom/sample/dataloader.py
@@ -6,10 +6,8 @@ def collate_fn(batch):
 
 def getDataloader(trainDataset, validDataset, batch, trainWorker, validWorker):
 
-
 	trainDataloader = DataLoader(trainDataset, batch_size=batch, shuffle=True, pin_memory=True, num_workers=trainWorker, collate_fn=collate_fn)
 	validDataloader = DataLoader(validDataset, batch_size=batch, shuffle=False,pin_memory=True, num_workers=validWorker, collate_fn=collate_fn)
-
 
 	return trainDataloader, validDataloader
 
--- baseline_code/custom/sample/model.py
+++ baseline_code/custom/sample/model.py
@@ -5,7 +5,7 @@ import segmentation_models_pytorch as smp
 def getModel():
 	
 	model = smp.Unet(
-			encoder_name="timm-efficientnet-b1",
+			encoder_name="resnet18",
 			encoder_weights="imagenet",
 			in_channels=3,
 			classes=11
