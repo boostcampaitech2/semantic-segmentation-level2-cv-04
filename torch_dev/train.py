21df4eaa1823f2a15cfed42537401d6e2cb9e4b5
ukcastle <jo631@naver.com>
2021-11-05 15:43:18 +0900

    docs(train.py): Add Annotation

--- torch_dev/train.py
+++ torch_dev/train.py
@@ -9,6 +9,7 @@ from utils.train_method import train
 from utils.set_seed import setSeed
 
 def getArgument():
+	# Custom 폴더 내 훈련 설정 목록을 선택
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--dir',type=str ,required=True)
 	return parser.parse_known_args()[0].dir
@@ -35,12 +36,13 @@ def main(custom_dir):
 
 	outputPath = os.path.join(arg.output_path, arg.custom_name)
 
+	#output Path 내 설정 저장
 	shutil.copytree(f"custom/{custom_dir}",outputPath)
 	os.makedirs(outputPath+"/models")
 	
 	# wandb
-	from utils.wandb_method import WandBMethod
 	if arg.wandb:
+			from utils.wandb_method import WandBMethod
 			WandBMethod.login(arg, model, criterion)
 
 	train(arg.epoch, model, trainLoader, valLoader, criterion, optimizer,scheduler, outputPath, arg.save_capacity, device, arg.wandb)
